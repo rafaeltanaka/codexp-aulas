26cfd1ccf88460c722c7d1cb55f8067d398218e4
RAFAEL YOSHIHIRO TANAKA <34667191864@jpmorgan.sp>
2018-01-11 21:56:59 -0200

    html e css cart e shipping. Início do js

--- Lógica de Programação/Revisão 2018/Core e Dom/cart.js
+++ Lógica de Programação/Revisão 2018/Core e Dom/cart.js
@@ -1,3 +1,49 @@
-let products = []
-let itens = '3 itens';
+let produtos = [
+    {
+        imagem: {
+            src: "./img/p1.png",
+            alt: "Camiseta roxa"
+        },
+        nome: "Puma Golf Raglan Tech Polo Tee",
+        cor: "Roxo",
+        preco: 72,
+        qtd: 3,
+    },
+    {
+        imagem: {
+            src: "./img/p2.png",
+            alt: "Camiseta roxa"
+        },
+        nome: "Nike Men´s Running Shoes",
+        cor: "Laranja",
+        preco: 129,
+        qtd: 1,
+    },
+    {
+        imagem: {
+            src: "./img/p3.png",
+            alt: "Camiseta roxa"
+        },
+        nome: "DC Men´s Axis Snowboard Jacket",
+        cor: "Azul",
+        preco: 89,
+        qtd: 2,
+    },
+];
+console.log(produtos);
+let itens = produtos.length;
 document.getElementById('itens__quantity').innerHTML = 'You have ' + itens + ' in your order';
+
+function listarProdutos() {
+    var template = "";
+    for (var i = 0; i < produtos.length; i++) {
+        var total = produtos[i].preco * produtos[i].qtd;
+        template += '<td class="table-border circle">x</td>';
+        template += '<td class="table-border" id="table-image"><img src="' + produtos[i].imagem.src + '" alt="' + produtos[i].imagem.alt + '"></td>';
+        template += '<td class="table-border" id="table-name">' + produtos[i].nome + '</td>';
+        template += '<td class="table-border" id="table-price">' + produtos[i].preco + '</td>';
+        template += '<td class="table-border" id="table-quantity">' + produtos[i].nome + '</td>';
+        template += '<td class="table-border" id="table-total">' + total + '</td>';
+    };
+    document.getElementById('products').innerHTML = template;
+};
