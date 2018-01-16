2053ddb83ce96b27e86d9b65a7e8c19a29afe146
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-01-16 21:57:36 -0200

    shipping - início do js

--- Lógica de Programação/Revisão 2018/Core e Dom/shipping.js
+++ Lógica de Programação/Revisão 2018/Core e Dom/shipping.js
@@ -1,3 +1,46 @@
-if (document.getElementsByClassName('input-standard').checked = true) {
-    document.getElementsByClassName('input-standard').color
-};
+// if (document.getElementsByClassName('input-standard').checked = true) {
+//     document.getElementsByClassName('input-standard').color
+// };
+var dadosCliente = {
+    endereco: {
+
+    },
+    contato: {
+
+    },
+}
+
+function validarFormulario() {
+    //gerar objeto endereco
+    event.preventDefault();
+    objEndereco = {
+        pais: document.getElementById('country').value,
+        estado: document.getElementById('state').value,
+        cep: document.getElementById('zip').value,
+        end: document.getElementById('address').value,
+        compliment: document.getElementById('compliment').value,
+    };
+    for (var property1 in objEndereco) {
+        if (objEndereco[property1] === '') {
+            alert('Preencha o  campo ' + property1);
+            alert(this.id);
+            return;
+        };
+    };
+
+    // if (objEndereco.cep === '') {
+    //     console.log("errado");
+    // } else {
+    //     console.log("certo");
+    // }
+    console.log(objEndereco);
+    //gerar objeto contato
+}
+
+function validarEndereco() {
+
+}
+
+function validarContato() {
+
+}
